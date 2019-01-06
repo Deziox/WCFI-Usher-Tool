b1769364fe4de3fb964079aafc165c21330e0464
deziox <dezioxe@gmail.com>
2019-01-06 12:09:50 -0500

    Added Append and Edit to Firestore database

--- WCFI People Info/LookupViewController.swift
+++ WCFI People Info/LookupViewController.swift
@@ -14,12 +14,32 @@ class LookupViewController: UIViewController {
 
     @IBOutlet weak var testLabel: UILabel!
     
+    @IBOutlet weak var testScroll: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func testDB(_ sender: UIButton) {
+    
+    @IBAction func testAdd(_ sender: UIButton) {
+        var ref: DocumentReference? = nil
+        let db = Firestore.firestore()
+        ref = db.collection("Members").addDocument(data: [
+            "First": "Danzel",
+            "Last": "Serrano",
+            "Email": "dezioxe@gmail.com"
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+            }
+        }
+        displayData()
+    }
+    
+    func displayData(){
         let db = Firestore.firestore()
         db.collection("Members").getDocuments(completion: { (querySnapshot, err) in
             if let err = err {
@@ -27,9 +47,15 @@ class LookupViewController: UIViewController {
             } else {
                 for document in querySnapshot!.documents {
                     self.testLabel.text = "\(document.documentID) => \(document.data()["Email"] ?? "")"
+                    let name = "\(document.data()["Last"] ?? ""), \(document.data()["First"] ?? "")"
+                    self.testScroll.text = self.testScroll.text.appending("\(document.documentID))\(name)\n\tEmail: \(document.data()["Email"] ?? "")\n")
                 }
             }
         })
+    }
+    
+    @IBAction func testDB(_ sender: UIButton) {
+        displayData()
     }
     
     /*
